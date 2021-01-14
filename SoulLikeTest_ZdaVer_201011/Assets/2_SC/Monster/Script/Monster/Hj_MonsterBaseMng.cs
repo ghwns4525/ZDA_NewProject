96ee90aebd5b9704b406263e9620c68ca97c35dd
ghwns4525 <63900477+ghwns4525@users.noreply.github.com>
2021-01-14 19:02:33 +0900

    1.14
    애니메이션 작업중

---
+++ SoulLikeTest_ZdaVer_201011/Assets/2_SC/Monster/Script/Monster/Hj_MonsterBaseMng.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MonsterState
+{
+    idle,       // 대기
+    boundary,   // 경계
+    chase,      // 추격
+    attack,     // 공격
+    comeback,   // 복귀
+    die,        // 죽음
+}
+
+public class Hj_MonsterBaseMng : MonoBehaviour
+{
+    [SerializeField]
+    float AttackDistance;
+    [SerializeField]
+    float ChaseDistance;
+    
+    public MonsterState monsterStateCheck = MonsterState.idle;
+
+    Hj_MonsterMoveAiHandler hj_MonsterMoveAiHandler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hj_MonsterMoveAiHandler = GetComponent<Hj_MonsterMoveAiHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MonsterStateChacker();
+    }
+
+    void MonsterStateChacker()
+    {
+        float dist = Vector3.Distance(hj_MonsterMoveAiHandler.TargetPlayer.transform.position, this.transform.position);
+        if (dist < AttackDistance)
+        {
+            // 몬스터의 상태를 공격으로 바꿈
+            monsterStateCheck = MonsterState.attack;
+        }
+        else if (dist > AttackDistance && dist < ChaseDistance)
+        {
+            // 몬스터의 상태를 추격으로 바꿈
+            monsterStateCheck = MonsterState.chase;
+        }
+        else if (this.transform.position.x > hj_MonsterMoveAiHandler.ChaseLimit.x ||
+            this.transform.position.z > hj_MonsterMoveAiHandler.ChaseLimit.z ||
+            this.transform.position.x < hj_MonsterMoveAiHandler.ChaseLimitC.x ||
+            this.transform.position.z < hj_MonsterMoveAiHandler.ChaseLimitC.z)
+        {
+            // 몬스터의 상태를 복귀로 바꿈
+            monsterStateCheck = MonsterState.comeback;
+        }
+        else if (hj_MonsterMoveAiHandler.IsPatrol && 
+            (this.transform.position.x < hj_MonsterMoveAiHandler.ChaseLimit.x ||
+            this.transform.position.z < hj_MonsterMoveAiHandler.ChaseLimit.z ||
+            this.transform.position.x > hj_MonsterMoveAiHandler.ChaseLimitC.x ||
+            this.transform.position.z > hj_MonsterMoveAiHandler.ChaseLimitC.z))
+        {
+            // 몬스터의 상태를 경계로 바꿈
+            monsterStateCheck = MonsterState.boundary;
+        }
+        else if(!hj_MonsterMoveAiHandler.IsPatrol)
+        {
+            // 몬스터의 상태를 대기로 바꿈
+            monsterStateCheck = MonsterState.idle;
+        }
+        
+        Debug.Log(monsterStateCheck);
+    }
+}
--- SoulLikeTest_ZdaVer_201011/Assets/2_SC/Monster/Script/Monster/Hj_MonsterMoveAiHandler.cs
+++ SoulLikeTest_ZdaVer_201011/Assets/2_SC/Monster/Script/Monster/Hj_MonsterMoveAiHandler.cs
@@ -10,16 +10,69 @@ public class Hj_MonsterMoveAiHandler : MonoBehaviour
     [SerializeField]
     bool isPatrol;
     [SerializeField]
-    bool isTrace;
-    [SerializeField]
     float walkSpeed;
     [SerializeField]
     float traceSpeed;
+    [SerializeField]
+    Vector3 startingPosition;
+    [SerializeField]
+    bool isStarting;
+    [SerializeField]
+    float chaseRange;
+    [SerializeField]
+    Vector3 chaseLimit;
+    [SerializeField]
+    Vector3 chaseLimitC;
 
-    private GameObject player;
+    bool isTrace;
+    
+
+    GameObject player;
     private Vector3 playerPoint;
 
     Hj_MonsterAnimationHandler hj_animationHandler;
+    Hj_MonsterBaseMng hj_MonsterBaseMng;
+
+    public GameObject TargetPlayer
+    {
+        get
+        {
+            return player;
+        }
+    }
+
+    public bool IsPatrol
+    {
+        get 
+        {
+            return isPatrol;
+        }
+    }
+
+
+    public Vector3 ChaseLimit
+    {
+        get
+        {
+            return chaseLimit;
+        }
+    }
+    public Vector3 ChaseLimitC
+    {
+        get
+        {
+            return chaseLimitC;
+        }
+    }
+
+    void Awake()
+    {
+        startingPosition = this.transform.position;
+        chaseLimit.x = startingPosition.x + chaseRange;
+        chaseLimit.z = startingPosition.z + chaseRange;
+        chaseLimitC.x = startingPosition.x - chaseRange;
+        chaseLimitC.z = startingPosition.z - chaseRange;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +80,7 @@ public class Hj_MonsterMoveAiHandler : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindWithTag("Player");
         hj_animationHandler = GetComponentInChildren<Hj_MonsterAnimationHandler>();
+        hj_MonsterBaseMng = GetComponent<Hj_MonsterBaseMng>();
     }
 
     // Update is called once per frame
@@ -34,61 +88,78 @@ public class Hj_MonsterMoveAiHandler : MonoBehaviour
     {
         playerPoint = player.transform.position;
 
+        Patrol(playerPoint);
+        Stop();
+        Chase(playerPoint);
+        Comeback(startingPosition);
 
-        if (isPatrol)
+        if (this.transform.position == startingPosition)
         {
-            Patrol(playerPoint);
+            isStarting = true;
         }
-        if (!isPatrol)
+        else
         {
-            Stop();
-        }
-        if (isTrace)
-        {
-            Trace(playerPoint);
+            isStarting = false;
         }
     }
 
-    void Trace(Vector3 target)
+    void Chase(Vector3 target)
     {
-        agent.isStopped = false;
-        agent.destination = target;
-        if(!isPatrol)
+        if(hj_MonsterBaseMng.monsterStateCheck == MonsterState.chase)
         {
+            agent.isStopped = false;
+            agent.destination = target;
             // 애니메이션을 walk로 넣어야할듯 뛰는게 안보임
-            hj_animationHandler.animator.SetFloat("Patrol", 0.1f);
-        }
-        // 속도 조절
-        agent.speed = traceSpeed;
-        Debug.Log(agent.speed);
+            hj_animationHandler.animator.SetFloat("PatrolY", 0.1f);
+            // 속도 조절
+            agent.speed = traceSpeed;
+            Debug.Log("추격");
+        }        
     }
 
     void Patrol(Vector3 target)
     {
-        if (!isTrace)
+        if(hj_MonsterBaseMng.monsterStateCheck == MonsterState.boundary)
         {
             agent.isStopped = false;
             // 목적지를 설정한다.(플레이어 혹은 일정한 지역이 될수도 있음)
             agent.destination = target;
 
             // 애니메이션은 blend값을 조절해서 멈추고 걷고 뛰게 조절
-            hj_animationHandler.animator.SetFloat("Patrol", 0.1f);
+            hj_animationHandler.animator.SetFloat("PatrolY", 0.1f);
 
             // 속도 조절
             agent.speed = walkSpeed;
-            Debug.Log(agent.speed);
-        }
+            Debug.Log("경계");
+        }        
     }
 
     void Stop()
     {
-        if(!isTrace)
+        if (hj_MonsterBaseMng.monsterStateCheck == MonsterState.idle)
         {
-            agent.isStopped = true;
-            // 이렇게 하고 애니메이션은 idle을 넣으면 순찰을 안하는 효과
-            //hj_animationHandler.PlayTargetActionAnimation("idle");
-            hj_animationHandler.animator.SetFloat("Patrol", 0);
-        }
-
+            Debug.Log("정지");
+            if (isStarting)
+            {
+                agent.isStopped = true;
+                // 이렇게 하고 애니메이션은 idle을 넣으면 순찰을 안하는 효과
+                //hj_animationHandler.PlayTargetActionAnimation("idle");
+                hj_animationHandler.animator.SetFloat("PatrolY", 0);
+            }
+        }          
     }
+
+    void Comeback(Vector3 target)
+    {
+        if (hj_MonsterBaseMng.monsterStateCheck == MonsterState.comeback)
+        {
+            if (!isStarting)
+            {
+                //isPatrol = false;
+                // 몬스터가 처음 있던 자리로 복귀
+                agent.destination = target;
+            }
+        }
+    }
+
 }
